8226d542d72a7c42000cd7a71797d7d93c57b4d5
Екатерина Лепесевич <13958@mail.ru>
2023-01-16 15:14:24 +0300

    Function 1 - hello world

--- 1.js
+++ 1.js
@@ -1,5 +1,5 @@
 //refer to the example and write your first JS function
 function helloWorld(){
-    var somewords="Hello World!"
-    console.log(somewords)
+    var str="Hello World!"
+    console.log(str)
   }
