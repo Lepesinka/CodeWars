5eb7a217ddd5561bb6e2ee2714732692c440a922
Екатерина Лепесевич <13958@mail.ru>
2023-01-19 13:50:30 +0300

    Basic Mathematical Operations

---
+++ 6.js
@@ -0,0 +1,20 @@
+function basicOp(operation, value1, value2)
+{ 
+  if (operation == "+")
+    {
+      return output = value1 + value2
+    }
+    if (operation == "-")
+    {
+      return output = value1 - value2
+    }
+    if (operation == "*")
+    {
+      return output = value1 * value2
+    }
+    if (operation == "/")
+    {
+      return output = value1 / value2
+    }
+  // Code
+}
