f239aa9d6606d5563b5dd86e715e98d5e7228223
Екатерина Лепесевич <13958@mail.ru>
2023-01-24 14:08:17 +0300

    Return Negative

---
+++ 22.js
@@ -0,0 +1,16 @@
+function makeNegative(num) {
+    if (num > 0 )
+      {
+        n = "-" + num 
+        nn = Number(n)
+        return nn
+      }
+      
+     
+    else 
+      {
+        return num
+    }    
+      
+      
+  }
