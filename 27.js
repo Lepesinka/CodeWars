ea3b3fd7d19304961ddef2ae158713431e3a4b45
Екатерина Лепесевич <13958@mail.ru>
2023-01-25 10:54:41 +0300

    Area or Perimeter

---
+++ 27.js
@@ -0,0 +1,12 @@
+const areaOrPerimeter = function(l , w) {
+    if (l==w)
+      {
+        return l*w
+      }
+    else
+      {
+        s = 2 * l +  w * 2
+        return s
+      }
+    // Return your answer
+  };
