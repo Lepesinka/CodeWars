7d3721139213a426efeb29cce611fdd48837d251
Екатерина Лепесевич <13958@mail.ru>
2023-01-30 14:43:43 +0300

    Find Maximum and Minimum Values of a List

---
+++ 38.js
@@ -0,0 +1,9 @@
+var min = function(list){
+    valueMin = Math.min.apply(null,list)
+    return valueMin    
+  }
+  
+  var max = function(list){
+    valueMax = Math.max.apply(null,list)
+    return valueMax    
+  }
