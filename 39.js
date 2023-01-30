f7bbe1e09fc513c38c47ca9667e5ee0441eead5c
Екатерина Лепесевич <13958@mail.ru>
2023-01-30 15:38:52 +0300

    Twice as old

---
+++ 39.js
@@ -0,0 +1,14 @@
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+    if (dadYearsOld >= sonYearsOld * 2)
+      {
+        ageNew = dadYearsOld - sonYearsOld * 2
+        return ageNew
+    }
+    else
+      {      
+        ageNew = sonYearsOld * 2 - dadYearsOld 
+        return ageNew 
+        
+        
+      }
+  }
