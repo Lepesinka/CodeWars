70861a9b8d02509b86261d8c162d65277b324878
Екатерина Лепесевич <13958@mail.ru>
2023-02-03 12:05:59 +0300

    Abbreviate a Two Word Name

---
+++ 48.js
@@ -0,0 +1,9 @@
+function abbrevName(name){
+    names = name.split(' ')
+    n = names[0].charAt(0).toUpperCase();
+    m = names[1].charAt(0).toUpperCase();
+    return n + '.'+ m
+  
+    
+  
+  }
